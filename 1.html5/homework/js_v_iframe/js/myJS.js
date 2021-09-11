38df3fa3560a88a8d4ad60e7644fa62aba899a38
eve <eve174428@gmail.com>
2021-09-11 13:02:56 +0800

    0911assignments

---
+++ 1.html5/homework/js_v_iframe/js/myJS.js
@@ -0,0 +1,31 @@
+function changeContent(file) {
+switch (file) {
+    case 'home':
+        document.getElementById("content").contentWindow.location.reload();
+        document.getElementById("content").src ="homePage.html" ;
+        document.title = "首頁";
+        break;
+    case 'about':
+        document.getElementById("content").contentWindow.location.reload();
+        document.getElementById("content").src ="about.html" ;
+        document.title = "關於我";
+        break;
+    case 'contact':
+        document.getElementById("content").contentWindow.location.reload();
+        document.getElementById("content").src ="contact.html" ;
+        document.title = "連絡我";
+        break;
+    case 'link':
+        document.getElementById("content").contentWindow.location.reload();
+        document.getElementById("content").src ="link.html" ;
+        document.title = "好站連結";
+        break;
+    case 'suggestion':
+        document.getElementById("content").contentWindow.location.reload();
+        document.getElementById("content").src ="suggestion.html" ;
+        document.title = "意見調查表";
+        break;
+    default:
+    break;
+}
+}
